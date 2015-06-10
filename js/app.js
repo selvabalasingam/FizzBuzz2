2d52b09c214b85db0938a96df60b66d793c7683a
Selva Balasingam <selva.balasingam@gmail.com>
2015-06-09 20:39:33 -0400

    initial commit

--- js/app.js
+++ js/app.js
@@ -6,13 +6,13 @@ $(document).ready(function(){
 		var div=$("<div>"); // creates a div element and instead of console.log you would have to use "div.text"
 		if(i%15 == 0) {
 			div.text("fizzbuzz");
-		}
+		} 
 		else if(i%3 == 0) {
 			div.text("fizz");	
 		} 
 		else if(i%5 == 0) {
 			div.text("buzz");	
-		}
+		} 
 		else{
 			div.text(i);
 		} 
@@ -21,11 +21,11 @@ $(document).ready(function(){
 	}
 		if(isNaN(convertNumber)){ // The isNaN() function determines whether a value is an illegal number (Not-a-Number).
 		prompt("Please enter a number only!");
-		}
-		else if(convertNumber % 1 == 0){ // this will send an alert if the input is a decimal
+		} 
+		else if(convertNumber % 1 > 0){ // if the remainder is greater than zero than it's a decimal, whereas if it was "convertNumber % 1 == 0" it would mean that there's no remainder (it's a whole number)
 		prompt("Please enter a whole number only!");
-		}
-		else{
+		} 
+		else {
 		fizzbuzzfunction();
 		}
 });
